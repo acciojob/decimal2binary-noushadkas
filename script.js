6d5e4b58cbb115debbd6169b1a3cf6be273e36cd
MOHAMMAD NOUSHAD <116102583+noushadkas@users.noreply.github.com>
2023-09-01 00:03:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-function decimalToBinary(num) {
-  //Write you code here
-  
+function decimalToBinary(decimalNumber) {
+    if (decimalNumber === 0) {
+        return '0';
+    }
+
+    let binary = '';
+    while (decimalNumber > 0) {
+        binary = (decimalNumber % 2) + binary;
+        decimalNumber = Math.floor(decimalNumber / 2);
+    }
+
+    return binary;
 }
 
-window.decimalToBinary = decimalToBinary;
+// Test cases
+console.log(decimalToBinary(7));   // Output: "111"
+console.log(decimalToBinary(10));  // Output: "1010"
+console.log(decimalToBinary(33));  // Output: "100001"
